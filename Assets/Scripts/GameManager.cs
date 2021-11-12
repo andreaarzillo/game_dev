29c0483890e360979bb544bac1b508e1dc57e995
Redimson Tommasina <redimson.tommasina@student.supsi.ch>
2021-11-12 17:27:05 +0100

    creazione top e bottom bar, creazione settings Menu popup e apertura/chiusura settings menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    // make sure the constructor is private, so it can only be instantiated here
+    private GameManager()
+    {
+        InputInteraction = false;
+    }
+
+    public bool InputInteraction { get; set; }
+
+    public static GameManager Instance { get; } = new GameManager();
+
+}
---
+++ Assets/Scripts/SettingsPopup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class SettingsPopup : MonoBehaviour
+{
+    
+    public void Open()
+    {
+        gameObject.SetActive(true); //object on top of the window
+        PauseControl.Instance.PauseGame(true);
+        GameManager.Instance.InputInteraction = false;
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false); //turn it off
+        PauseControl.Instance.PauseGame(false);
+        GameManager.Instance.InputInteraction = true;
+    }
+
+    public void Submit()
+    {
+        //OnSubmitName(playerName.text);
+        Close();
+    }
+
+
+}
